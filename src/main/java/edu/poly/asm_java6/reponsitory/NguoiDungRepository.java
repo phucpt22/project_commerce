beb998f3921157613e775bc68cab08b4bb6a6cc7
Tuuuuuuuuuuuuuuuuuuuuuuuuuuuu <ngtranhtu.96@gmail.com>
2023-03-26 20:22:25 +0700

    test

--- src/main/java/edu/poly/asm_java6/reponsitory/NguoiDungRepository.java
+++ src/main/java/edu/poly/asm_java6/reponsitory/NguoiDungRepository.java
@@ -4,4 +4,6 @@ import edu.poly.asm_java6.entities.NguoiDung;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NguoiDungRepository extends JpaRepository<NguoiDung, Integer> {
+
+    // testtttttttttttttttttttttttttttttt
 }
